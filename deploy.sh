f9cf4d56d48f5cd9cd68d9656145d5e4c1f5ab5d
ethandeveloper2 <ethandeveloper2@gmail.com>
2025-05-23 00:06:44 +0900

    [Feat] CI/CD 자동화, deploy.sh 파일 경로 재수정

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-
-# 에러 발생 시 스크립트 중단
 set -e
+set -x  # 디버깅 로그 출력
 
-# 서버/클라이언트 빌드 파일이 복사된 위치로 이동
 cd /home/ubuntu/mime-game
 
-# (선택) 의존성 설치 (필요시)
-# cd mime-game-server
-# npm ci --only=production
-
 # 서버 실행 (NestJS)
-pm2 reload my-server || pm2 start mime-game-server/dist/main.js --name my-server --env PORT=3000
+PORT=3000 pm2 reload my-server || PORT=9100 pm2 start mime-game-server/dist/main.js --name my-server
 
 # 클라이언트 실행 (Next.js)
-pm2 reload my-client || pm2 start "npm" --name my-client -- start --prefix mime-game-client
+pm2 reload my-client || pm2 start "npm start --prefix mime-game-client" --name my-client
 
-echo "배포 완료!"
+echo "✅ 배포 완료!"
