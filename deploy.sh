b7b06a437a2ed208b58341d10c9c4bce59b5a78c
ethandeveloper2 <ethandeveloper2@gmail.com>
2025-05-22 22:27:35 +0900

    [Feat] CI/CD 자동화, deploy.yml 파일 경로 재수정

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,16 @@
 set -e
 
 # 서버/클라이언트 빌드 파일이 복사된 위치로 이동
-cd /home/ubuntu/mime-game-deploy
+cd /home/ubuntu/mime-game
 
 # (선택) 의존성 설치 (필요시)
 # cd mime-game-server
 # npm ci --only=production
 
-# pm2로 서버 재시작 (my-app-1, my-app-2는 pm2에서 관리하는 이름)
-pm2 reload my-app-1 || pm2 start dist/main.js --name my-app-1 --env PORT=3000
-pm2 reload my-app-2 || pm2 start dist/main.js --name my-app-2 --env PORT=3001
+# 서버 실행 (NestJS)
+pm2 reload my-server || pm2 start mime-game-server/dist/main.js --name my-server --env PORT=3000
+
+# 클라이언트 실행 (Next.js)
+pm2 reload my-client || pm2 start "npm" --name my-client -- start --prefix mime-game-client
 
 echo "배포 완료!"
