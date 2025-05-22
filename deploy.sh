c98c8b325a4b6607112d7f65465a36a8e079143c
ethandeveloper2 <ethandeveloper2@gmail.com>
2025-05-23 00:59:22 +0900

    [Feat] CI/CD 자동화, pm2 start 시 에러 수정

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,20 @@ set -x  # 디버깅 로그 출력
 
 cd /home/ubuntu/mime-game
 
-# 서버 실행 (NestJS)
-PORT=3000 pm2 reload my-server || PORT=9100 pm2 start mime-game-server/dist/main.js --name my-server
+# 1. 서버 의존성 설치 (NestJS)
+cd mime-game-server
+npm install --production
+cd ..
 
-# 클라이언트 실행 (Next.js)
+# 2. 클라이언트 의존성 설치 (Next.js)
+cd mime-game-client
+npm install --production
+cd ..
+
+# 3. 서버 실행 (NestJS)
+PORT=9100 pm2 reload my-server || PORT=9100 pm2 start mime-game-server/dist/main.js --name my-server
+
+# 4. 클라이언트 실행 (Next.js)
 pm2 reload my-client || pm2 start "npm start --prefix mime-game-client" --name my-client
 
 echo "✅ 배포 완료!"
